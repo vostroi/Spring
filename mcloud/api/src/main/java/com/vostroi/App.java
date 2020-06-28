f0057f6fe68f80acc039d77e15a6929f0466e59b
TK <1009022110@QQ.COM>
2020-06-28 11:19:28 +0800

    搭建项目结构 mcloud |-api     |-各服务API |-core |-micro-services     |-各服务     |-     |-

---
+++ mcloud/api/src/main/java/com/vostroi/App.java
@@ -0,0 +1,13 @@
+package com.vostroi;
+
+/**
+ * Hello world!
+ * @author vostroi
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
