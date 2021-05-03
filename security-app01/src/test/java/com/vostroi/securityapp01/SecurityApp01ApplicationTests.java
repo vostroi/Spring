ac03604bb5969307595da6d0d8c45c35c1ab430f
TK <1009022110@QQ.COM>
2021-05-04 02:55:26 +0800

    作为单点登录的客户端应用，测试单点登录未通过

---
+++ security-app01/src/test/java/com/vostroi/securityapp01/SecurityApp01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vostroi.securityapp01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApp01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
